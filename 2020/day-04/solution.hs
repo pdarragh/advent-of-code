5307598bb53c682de9387410120b979616bb003b
Pierce Darragh <pierce.darragh@gmail.com>
2020-12-07 00:30:13 -0700

    day-04: implement part 2 solution

--- 2020/day-04/solution.hs
+++ 2020/day-04/solution.hs
@@ -1,11 +1,49 @@
 import Control.Monad (liftM2, replicateM)
-import Data.Char (isSpace)
+import Data.Char (isDigit, isSpace)
+import Data.List (intercalate)
+import Data.Maybe (fromJust)
 import Data.Tuple (swap)
-import Text.ParserCombinators.ReadP (ReadP, (<++), char, get, many1, munch1, pfail, sepBy1)
+import Text.ParserCombinators.ReadP
+  ( ReadP, readP_to_S, (<++)
+  , char, choice, get, many1, munch1, pfail, satisfy, sepBy1, string
+  )
 import Text.Read (readPrec, readP_to_Prec)
 
 sourceFile :: String
 sourceFile = "input.txt"
+
+-- Like `ReadP.get` but gathers n characters into a string.
+getN :: Int -> ReadP String
+getN n = replicateM n get
+
+-- Like `ReadP.many` but requires at least 2 occurrences of *p*.
+many2 :: ReadP a -> ReadP [a]
+many2 p = liftM2 (:) p (many1 p)
+
+-- Run a parser without a Read instance. The `readP_to_S` function used returns
+-- a list. We consider the parser successful only if there exists exactly one
+-- pair in the list whose second element (the un-parsed remaining string)
+-- consists entirely of whitespace or newline characters.
+runP_to_a :: ReadP a -> String -> Maybe a
+runP_to_a p s = case results of
+  [(result, "")] -> Just result
+  _              -> Nothing
+  where
+    results = filter (all isSpace . snd) (readP_to_S p s)
+
+-- Runs a parser and returns True if the parse succeeded, otherwise False.
+runP_to_Bool :: ReadP a -> String -> Bool
+runP_to_Bool p s = boolFromMaybe (True <$ runP_to_a p s)
+
+-- Runs a parser and, if the parse was successful, validates the result using
+-- the supplied function.
+validateP_as_Bool :: ReadP a -> (a -> Bool) -> String -> Bool
+validateP_as_Bool p f s = boolFromMaybe (f <$> runP_to_a p s)
+
+-- Minimizes a trinary Maybe Bool to a binary Bool.
+boolFromMaybe :: Maybe Bool -> Bool
+boolFromMaybe Nothing = False
+boolFromMaybe (Just b) = b
 
 -- Only these fields are valid in passports.
 data Field
@@ -32,17 +70,13 @@ fieldLabels =
   , (CountryID,      "cid")
   ]
 
+instance Show Field where
+  -- Renders a field as its label.
+  show f = fromJust (lookup f fieldLabels)
+
 -- Sometimes we want to lookup in `fieldLabels` by code instead of field.
 reverseLookup :: Eq b => b -> [(a, b)] -> Maybe a
 reverseLookup key pairs = lookup key (map swap pairs)
-
--- Like `ReadP.get` but gathers n characters into a string.
-getN :: Int -> ReadP String
-getN n = replicateM n get
-
--- Like `ReadP.many` but requires at least 2 occurrences of *p*.
-many2 :: ReadP a -> ReadP [a]
-many2 p = liftM2 (:) p (many1 p)
 
 -- Defines a parser for reading a field. Only the defined fields are supported.
 readField :: ReadP Field
@@ -62,6 +96,12 @@ instance Read Field where
 --       type instead.
 data Specification = Specification { getField :: Field
                                    , getValue :: String }
+                     deriving (Eq)
+
+instance Show Specification where
+  -- Renders a specification as the field label with its value, separated by a
+  -- colon.
+  show s = show (getField s) ++ ":" ++ getValue s
 
 -- Defines a parser for reading a specification. A specification consists of a
 -- field code followed by a colon and then a string of non-space characters.
@@ -77,7 +117,11 @@ instance Read Specification where
   readPrec = readP_to_Prec (const readSpecification)
 
 -- A passport is just a collection of specifications.
-newtype Passport = Passport [Specification]
+newtype Passport = Passport [Specification] deriving (Eq)
+
+instance Show Passport where
+  -- Renders a passport as its specifications, each on its own line.
+  show (Passport specs) = unlines (map show specs)
 
 -- Defines a parser for a passport. Specifications can be separated by spaces or
 -- newlines, but only single newlines are allowed.
@@ -91,7 +135,11 @@ instance Read Passport where
   readPrec = readP_to_Prec (const readPassport)
 
 -- A collection of passports. This exists specifically to enable `read`ing.
-newtype Passports = Passports [Passport]
+newtype Passports = Passports [Passport] deriving (Eq)
+
+instance Show Passports where
+  -- Renders a collection of passports each separated by a blank line.
+  show (Passports ps) = intercalate "\n\n" (map show ps)
 
 -- Defines a parser for reading passports. Consecutive passports are separated
 -- by one or more blank lines (two or more consecutive newline characters).
@@ -104,23 +152,51 @@ instance Read Passports where
   -- Reads a collection of passports from a string.
   readPrec = readP_to_Prec (const readPassports)
 
--- These fields *must* appear in a valid passport.
-requiredFields :: [Field]
-requiredFields =
-  [ BirthYear
-  , IssueYear
-  , ExpirationYear
-  , Height
-  , HairColor
-  , EyeColor
-  , PassportID
-  ]
+-- Defines the bounds for the supported height units.
+heightBounds :: String -> (Int, Int)
+heightBounds "cm" = (150, 193)
+heightBounds "in" = (59, 76)
+heightBounds s    = error ("invalid height unit: '" ++ s ++ "'")
+
+-- Reads a height from a string, producing a pair consisting of the unit and
+-- the value. The value is an un-validated non-negative integer.
+readHeight :: ReadP (String, Int)
+readHeight = do
+  num  <- munch1 isDigit
+  unit <- choice [string "cm", string "in"]
+  return (unit, read num)
+
+-- Defines a validation function for each required field. These are run by
+-- providing the field value as a string. Many of them are just parsers.
+requiredFieldValidators :: [(Field, String -> Bool)]
+requiredFieldValidators =
+  [ (BirthYear,      intBound 1920 2002 . read)
+  , (IssueYear,      intBound 2010 2020 . read)
+  , (ExpirationYear, intBound 2020 2030 . read)
+  , (Height,         validateP_as_Bool readHeight (\(unit, val) -> uncurry intBound (heightBounds unit) val))
+  , (HairColor,      runP_to_Bool (char '#' >> replicateM 6 (satisfy (\c -> isDigit c || elem c "abcdef"))))
+  , (EyeColor,       runP_to_Bool (choice (map string ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])))
+  , (PassportID,     runP_to_Bool (replicateM 9 (satisfy isDigit)))
+  ] where
+  intBound :: Int -> Int -> Int -> Bool
+  intBound lo hi val = val >= lo && val <= hi
 
 -- Simple (part 1) passport validation checks only whether all required fields
 -- are present.
-isPassportValid :: Passport -> Bool
-isPassportValid (Passport specifications) =
-  all (`elem` map getField specifications) requiredFields
+passportContainsRequiredFields :: Passport -> Bool
+passportContainsRequiredFields (Passport specifications) =
+  all (`elem` map getField specifications) (map fst requiredFieldValidators)
+
+-- More complex (part 2) passport validation. After checking whether all
+-- required fields are present, validates those fields' values.
+passportFieldsHaveValidValues :: Passport -> Bool
+passportFieldsHaveValidValues p@(Passport specifications) =
+  passportContainsRequiredFields p &&
+  all isSpecValid specifications where
+    isSpecValid :: Specification -> Bool
+    isSpecValid spec = case lookup (getField spec) requiredFieldValidators of
+      Nothing       -> True
+      Just validate -> validate (getValue spec)
 
 -- Converts a file into a list of passports.
 readInputFile :: String -> IO [Passport]
@@ -132,4 +208,5 @@ readInputFile fileName = do
 main :: IO ()
 main = do
   passports <- readInputFile sourceFile
-  putStrLn ("Counted " ++ show (length (filter isPassportValid passports)) ++ " valid passports.")
+  putStrLn (show (length (filter passportContainsRequiredFields passports)) ++ " passports contain all required fields.")
+  putStrLn (show (length (filter passportFieldsHaveValidValues passports)) ++ " of those passports are completely valid.")
