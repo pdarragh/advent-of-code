b56ab8f746b0a2e4e6b9f35e1e429e2a1d31d1b6
Pierce Darragh <pierce.darragh@gmail.com>
2020-12-12 01:11:08 -0700

    day-07: implement parsing and state handling

---
+++ 2020/day-07/solution.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Control.Monad.State hiding (get)
+import Data.Char (isDigit)
+import Data.Maybe (fromJust)
+import Text.ParserCombinators.ReadP (
+  ReadP,
+  char, choice, get, many1, manyTill, munch1, optional, satisfy, sepBy, skipSpaces, string)
+import Text.Read (readPrec, readP_to_Prec)
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.Map.Strict as Map
+
+sourceFile :: String
+sourceFile = "input.txt"
+
+(<??>) :: Monad m => m (Maybe a) -> m a -> m a
+(<??>) mmx dx = do
+  mx <- mmx
+  case mx of
+    Nothing -> dx
+    Just x  -> return x
+
+(*->) :: s -> State s a -> a
+(*->) = flip evalState
+
+type Color = String
+type ColorID = Int
+type ColorToIDMap = Map.Map Color ColorID
+type IDToColorMap = IntMap.IntMap Color
+
+data MapState = MapState { colorsToIDs :: ColorToIDMap
+                         , idsToColors :: IDToColorMap
+                         , nextID      :: ColorID }
+                deriving (Show)
+
+initialState :: MapState
+initialState = MapState { colorsToIDs = Map.empty
+                        , idsToColors = IntMap.empty
+                        , nextID      = 0 }
+
+addColor' :: Color -> MapState -> MapState
+addColor' color MapState{..} = MapState { colorsToIDs = Map.insert color nextID colorsToIDs
+                                        , idsToColors = IntMap.insert nextID color idsToColors
+                                        , nextID      = nextID + 1 }
+
+addColor :: Color -> State MapState Int
+addColor color = gets nextID <* modify (addColor' color)
+
+lookupColor :: Color -> State MapState (Maybe ColorID)
+lookupColor color = Map.lookup color <$> gets colorsToIDs
+
+colorToIDOrAdd :: Color -> State MapState Int
+colorToIDOrAdd color = lookupColor color <??> addColor color
+
+idToColor :: ColorID -> State MapState Color
+idToColor colorID = fromJust . IntMap.lookup colorID <$> gets idsToColors
+
+data RawContainmentRule = RawContainmentRule Color [(Int, Color)] deriving (Eq, Show)
+
+readRawContainmentRule :: ReadP RawContainmentRule
+readRawContainmentRule = do
+  color <- many1 get
+  _     <- string " bags contain "
+  rules <- choice [string "no other bags" >> return [], readBags]
+  _     <- char '.'
+  return (RawContainmentRule color rules)
+  where
+    readBags :: ReadP [(Int, Color)]
+    readBags = sepBy readBag (char ',' >> skipSpaces)
+    readBag :: ReadP (Int, Color)
+    readBag = do
+      n <- munch1 isDigit
+      _ <- skipSpaces
+      c <- manyTill (satisfy (\c -> c /= ',' && c /= '.')) (string "bag")
+      _ <- optional (char 's')
+      return (read n, c)
+
+instance Read RawContainmentRule where
+  readPrec = readP_to_Prec (const readRawContainmentRule)
+
+data ContainmentRule = ContainmentRule ColorID [(Int, ColorID)] deriving (Show)
+
+convertRule :: RawContainmentRule -> State MapState ContainmentRule
+convertRule (RawContainmentRule color rules) = do
+  colorID <- colorToIDOrAdd color
+  let (quantities, colors) = unzip rules
+  colorIDs <- mapM colorToIDOrAdd colors
+  return (ContainmentRule colorID (zip quantities colorIDs))
+
+readInputFile :: String -> IO [RawContainmentRule]
+readInputFile fileName = do
+  content <- readFile fileName
+  return (map read (lines content))
+
+main :: IO ()
+main = do
+  rawRules <- readInputFile sourceFile
+  let rules = initialState *-> mapM convertRule rawRules
+  mapM_ print (zip [1..] rules)
