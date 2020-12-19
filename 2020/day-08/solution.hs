8740404c80dfef54fbd3827965a0f5b670fed2d3
Pierce Darragh <pierce.darragh@gmail.com>
2020-12-18 19:12:57 -0700

    day-08: implement parsing

--- 2020/day-08/solution.hs
+++ 2020/day-08/solution.hs
@@ -1,2 +1,61 @@
+import Control.Applicative ((<|>))
+import Data.Bool (bool)
+import Data.Char (isDigit, toLower)
+import Data.Functor (($>))
+import Text.ParserCombinators.ReadP (ReadP, char, munch1, sepBy, skipSpaces, string)
+import Text.Read (readListPrec, readPrec, readP_to_Prec)
+
+data InstructionType
+  = Acc
+  | Jmp
+  | Nop
+  deriving (Eq, Show)
+
+readInstructionType :: ReadP InstructionType
+readInstructionType = Acc <$ string "acc" <|>
+                      Jmp <$ string "jmp" <|>
+                      Nop <$ string "nop"
+
+instance Read InstructionType where
+  readPrec = readP_to_Prec (const readInstructionType)
+
+newtype InstructionValue = InstructionValue Int deriving (Eq, Show)
+
+readInstructionValue :: ReadP InstructionValue
+readInstructionValue = do
+  sign <- char '+' $>   1  <|>
+          char '-' $> (-1)
+  rawNum <- munch1 isDigit
+  let num = (read :: String -> Int) rawNum
+  return (InstructionValue (sign * num))
+
+instance Read InstructionValue where
+  readPrec = readP_to_Prec (const readInstructionValue)
+
+data Instruction = Instruction InstructionType InstructionValue
+
+instance Show Instruction where
+  show (Instruction t (InstructionValue v)) = map toLower (show t) ++ " " ++ bool "-" "+" (v < 0) ++ show v
+
+readInstruction :: ReadP Instruction
+readInstruction = Instruction <$> readInstructionType <* skipSpaces <*> readInstructionValue
+
+readInstructions :: ReadP [Instruction]
+readInstructions = sepBy readInstruction skipSpaces
+
+instance Read Instruction where
+  readPrec = readP_to_Prec (const readInstruction)
+  readListPrec = readP_to_Prec (const readInstructions)
+
+readInputFile :: String -> IO [Instruction]
+readInputFile fileName = do
+  content <- readFile fileName
+  return (read content)
+
+sourceFile :: String
+sourceFile = "input.txt"
+
 main :: IO ()
-main = putStrLn "day-08"
+main = do
+  instructions <- readInputFile sourceFile
+  mapM_ print instructions
