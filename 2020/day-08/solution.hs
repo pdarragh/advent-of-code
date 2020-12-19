c98700dee4b4508e2e0e6af7b56d65c3130be95e
Pierce Darragh <pierce.darragh@gmail.com>
2020-12-18 21:26:47 -0700

    day-08: add basic state management stuff

--- 2020/day-08/solution.hs
+++ 2020/day-08/solution.hs
@@ -1,4 +1,5 @@
 import Control.Applicative ((<|>))
+import Control.Monad.State
 import Data.Bool (bool)
 import Data.Char (isDigit, toLower)
 import Data.Functor (($>))
@@ -46,6 +47,40 @@ readInstructions = sepBy readInstruction skipSpaces
 instance Read Instruction where
   readPrec = readP_to_Prec (const readInstruction)
   readListPrec = readP_to_Prec (const readInstructions)
+
+-- IPU = Instruction Processing Unit, because I am very clever.
+data IPUState = IPUState { ipuAcc :: Int
+                         , ipuIp  :: Int }
+
+initialState :: IPUState
+initialState = IPUState 0 0
+
+initializeIPU :: State IPUState a -> (a, IPUState)
+initializeIPU = flip runState initialState
+
+currentAcc :: State IPUState Int
+currentAcc = gets ipuAcc
+
+currentIp :: State IPUState Int
+currentIp = gets ipuIp
+
+updateAcc :: Int -> State IPUState ()
+updateAcc newAcc = get >>= \s -> put s { ipuAcc = newAcc + ipuAcc s }
+
+updateIp :: Int -> State IPUState ()
+updateIp newIp = get >>= \s -> put s { ipuIp = newIp }
+
+updateIpByOffset :: Int -> State IPUState ()
+updateIpByOffset off = (+ off) <$> currentIp >>= updateIp
+
+incrementIp :: State IPUState ()
+incrementIp = updateIpByOffset 1
+
+executeInstruction :: Instruction -> State IPUState ()
+executeInstruction (Instruction t (InstructionValue v)) = case t of
+  Acc -> updateAcc v >> incrementIp
+  Jmp -> updateIpByOffset v
+  Nop -> incrementIp
 
 readInputFile :: String -> IO [Instruction]
 readInputFile fileName = do
