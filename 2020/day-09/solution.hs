b48f4a5716fa902ce09d8c2b26ae679e535af583
Pierce Darragh <pierce.darragh@gmail.com>
2020-12-21 17:23:37 -0700

    day-09: finish part 2

--- 2020/day-09/solution.hs
+++ 2020/day-09/solution.hs
@@ -1,9 +1,16 @@
 import Data.List (tails)
+import Data.Sequence ((|>), Seq(..))
 
 -- Safely produces the head of a list, if it exists.
 maybeHead :: [a] -> Maybe a
 maybeHead []    = Nothing
 maybeHead (x:_) = Just x
+
+-- Unsafely produces the right tail of a sequence (the sequence without the left
+-- head), raising an error if the sequence is empty.
+rightTail :: Seq a -> Seq a
+rightTail Empty = error "empty sequence"
+rightTail (_:<|t) = t
 
 -- Given a target integer and a pair of integers, determines whether the target
 -- is equal to the sum of the pair.
@@ -44,6 +51,17 @@ elemsNotExpressibleAsPairsInWindows f len xs = map fst (filter (not . uncurry (e
 numbersNotExpressibleAsPairsInWindows :: Int -> [Int] -> [Int]
 numbersNotExpressibleAsPairsInWindows = elemsNotExpressibleAsPairsInWindows (==.+)
 
+-- Attempts to find the contiguous subsequence of a list of numbers that sum to
+-- a given target value.
+sublistSum :: Int -> [Int] -> Seq Int
+sublistSum target = sublistSum' Empty
+  where
+    sublistSum' :: Seq Int -> [Int] -> Seq Int
+    sublistSum' buffer ns = case compare (sum buffer) target of
+      EQ -> buffer
+      LT -> sublistSum' (buffer |> head ns) (tail ns)
+      GT -> sublistSum' (rightTail buffer) ns
+
 -- Converts the input file to a list of integers.
 readInputFile :: String -> IO [Int]
 readInputFile fileName = do
@@ -61,4 +79,10 @@ main = do
   nums <- readInputFile sourceFile
   case maybeHead (numbersNotExpressibleAsPairsInWindows windowSize nums) of
     Nothing -> putStrLn "All numbers are expressible as pairs in their preceding windows."
-    Just x  -> putStrLn ("The first number not expressible as a pair in its preceding window is: " ++ show x)
+    Just x  -> do
+      putStrLn ("The first number not expressible as a pair in its preceding window is: " ++ show x)
+      let sumSequence = sublistSum x nums
+          lo = minimum sumSequence
+          hi = maximum sumSequence
+          pairSum = lo + hi
+      putStrLn ("The sum of the first and last elements of the subsequence that sums to that number is: " ++ show pairSum)
