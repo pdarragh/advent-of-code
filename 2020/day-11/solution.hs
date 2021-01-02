a626c4267eea56e8d61a40240db0925e18655981
Pierce Darragh <pierce.darragh@gmail.com>
2021-01-02 12:16:16 -0700

    day-11: stage in-progress work

--- 2020/day-11/solution.hs
+++ 2020/day-11/solution.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE RecordWildCards #-}
-
 import Control.Applicative ((<|>))
-import Text.ParserCombinators.ReadP
+import Data.List (intercalate)
+import Text.ParserCombinators.ReadP (ReadP, char, many1)
 import Text.Read (readListPrec, readPrec, readP_to_Prec)
+
+import Debug.Trace (trace)
 
 data CellState
   = Floor
@@ -11,8 +12,8 @@ data CellState
   deriving (Eq)
 
 instance Show CellState where
-  show Floor = "."
-  show Empty = "L"
+  show Floor    = "."
+  show Empty    = "L"
   show Occupied = "#"
 
 readCellState :: ReadP CellState
@@ -27,24 +28,103 @@ instance Read CellState where
   readPrec = readP_to_Prec (const readCellState)
   readListPrec = readP_to_Prec (const readCellStates)
 
-data Coordinate = Coordinate { getX :: Int
-                             , getY :: Int }
-                  deriving (Eq)
+type Coord = (Int, Int)
 
-instance Show Coordinate where
-  show Coordinate{..} = "(" ++ show getX ++ ", " ++ show getY ++ ")"
+data Grid = Grid { height :: Int
+                 , width  :: Int
+                 , cells  :: [CellState] }
+  deriving (Eq)
 
-data Seat = Seat CellState Coordinate deriving (Eq, Show)
+instance Show Grid where
+  show g = intercalate "\n" (map (concatMap show) (gridRows (cells g)))
+    where
+      gridRows :: [CellState] -> [[CellState]]
+      gridRows [] = []
+      gridRows cs = row : gridRows cs'
+        where
+          (row, cs') = splitAt (width g) cs
 
-readInputFile :: String -> IO [[CellState]]
+validWidth :: [[CellState]] -> Int
+validWidth [] = 0
+validWidth (firstRow:rows) = shapeValid' (length firstRow) rows
+  where
+    shapeValid' :: Int -> [[CellState]] -> Int
+    shapeValid' w [] = w
+    shapeValid' w (row:rows')
+      | length row == w = shapeValid' w rows'
+      | otherwise       = error "inconsistent row widths"
+
+makeGrid :: [[CellState]] -> Grid
+makeGrid rows = Grid { height = h
+                     , width  = w
+                     , cells  = concat rows }
+  where
+    h = length rows
+    w = validWidth rows
+
+coords :: Grid -> [Coord]
+coords g = [(x, y) | y <- [0..height g - 1]
+                   , x <- [0..width  g - 1]]
+
+getCell :: Grid -> Coord -> CellState
+getCell g c = cells g !! coordToIndex g c
+
+coordToIndex :: Grid -> Coord -> Int
+coordToIndex g (x, y) = y + x * width g
+
+validCoord :: Grid -> Coord -> Bool
+validCoord g (x, y)
+  | x < 0 || x >= width  g = False
+  | y < 0 || y >= height g = False
+  | otherwise              = True
+
+getNeighbors :: Grid -> Coord -> [CellState]
+getNeighbors g (x, y) = filteredNeighbors
+  where
+    filteredNeighbors :: [CellState]
+    filteredNeighbors = map (cells g !!) filteredIndices
+    filteredIndices :: [Int]
+    filteredIndices = map (coordToIndex g) filteredCoords
+    filteredCoords :: [Coord]
+    filteredCoords = filter validNeighborCoord neighborCoords
+    neighborCoords :: [Coord]
+    neighborCoords = [(x', y') | y' <- map (+ y) [-1..1]
+                               , x' <- map (+ x) [-1..1]]
+    validNeighborCoord :: Coord -> Bool
+    validNeighborCoord c = validCoord g c && c /= (x, y)
+
+updateSeats :: Grid -> Grid
+updateSeats g
+  | updatedGrid /= g = trace ("updated grid:\n" ++ show updatedGrid ++ "\n\n") updateSeats updatedGrid
+  | otherwise        = updatedGrid
+  where
+    updatedGrid :: Grid
+    updatedGrid = g{cells=fixSeats (coords g)}
+    fixSeats :: [Coord] -> [CellState]
+    fixSeats [] = []
+    fixSeats (c:cs) = updatedSeat : fixSeats cs
+      where
+        updatedSeat :: CellState
+        updatedSeat
+          | oldSeat == Empty    && notElem Empty oldNeighbors                      = Occupied
+          | oldSeat == Occupied && length (filter (== Occupied) oldNeighbors) >= 4 = Empty
+          | otherwise = oldSeat
+        oldSeat :: CellState
+        oldSeat = getCell g c
+        oldNeighbors :: [CellState]
+        oldNeighbors = getNeighbors g c
+
+readInputFile :: String -> IO Grid
 readInputFile fileName = do
   content <- readFile fileName
-  return (map read (lines content))
+  return (makeGrid (map read (lines content)))
 
 sourceFile :: String
-sourceFile = "input.txt"
+sourceFile = "input2.txt"
 
 main :: IO ()
 main = do
-  cells <- readInputFile sourceFile
-  mapM_ print cells
+  grid <- readInputFile sourceFile
+  let updatedGrid = trace ("original grid:\n" ++ show grid ++ "\n\n") updateSeats grid
+      occupiedSeats = length (filter (== Occupied) (cells updatedGrid))
+  putStrLn ("Number of seats occupied at the end: " ++ show occupiedSeats)
