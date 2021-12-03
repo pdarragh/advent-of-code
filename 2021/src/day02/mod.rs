6a19fc9371656b24ef256c34f4389488510ef922
Pierce Darragh <pdarragh@mbp16inch2021.myfiosgateway.com>
2021-12-02 23:03:33 -0500

    2021/src/day02: implement part 2

--- 2021/src/day02/mod.rs
+++ 2021/src/day02/mod.rs
@@ -4,25 +4,6 @@ use std::fs;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-struct Position {
-    horizontal: i32,
-    depth: i32
-}
-
-impl Position {
-    fn increase_horizontal(&mut self, x: i32) {
-        self.horizontal += x;
-    }
-
-    fn increase_depth(&mut self, d: i32) {
-        self.depth += d;
-    }
-
-    fn decrease_depth(&mut self, d: i32) {
-        self.depth -= d;
-    }
-}
-
 enum Command {
     Forward(i32),
     Down(i32),
@@ -43,18 +24,54 @@ fn commands_from_file(file: &fs::File) -> Vec<Command> {
     commands
 }
 
-pub fn part1(file: &fs::File) -> String {
-    let mut pos = Position { horizontal: 0, depth: 0 };
-    for command in commands_from_file(file) {
+struct Position {
+    horizontal: i32,
+    depth: i32,
+}
+
+impl Position {
+    fn eval_command(&mut self, command: &Command) {
         match command {
-            Command::Forward(x) => { pos.increase_horizontal(x) }
-            Command::Down(d) => { pos.increase_depth(d) }
-            Command::Up(d) => { pos.decrease_depth(d) }
+            Command::Forward(x) => { self.horizontal += x }
+            Command::Down(d) => { self.depth += d }
+            Command::Up(d) => { self.depth -= d }
         }
     }
+}
+
+pub fn part1(file: &fs::File) -> String {
+    let mut pos = Position { horizontal: 0, depth: 0 };
+    commands_from_file(file)
+        .iter()
+        .for_each(|c| pos.eval_command(c));
     return (pos.horizontal * pos.depth).to_string();
 }
 
+struct Aim {
+    pos: Position,
+    aim: i32,
+}
+
+impl Aim {
+    fn eval_command(&mut self, command: &Command) {
+        match command {
+            Command::Forward(x) => { self.pos.horizontal += x;
+                                     self.pos.depth += self.aim * x }
+            Command::Down(d) => { self.aim += d }
+            Command::Up(d) => { self.aim -= d }
+        }
+    }
+}
+
+pub fn part2(file: &fs::File) -> String {
+    let pos = Position { horizontal: 0, depth: 0 };
+    let mut aim = Aim { pos, aim: 0 };
+    commands_from_file(file)
+        .iter()
+        .for_each(|c| aim.eval_command(c));
+    return (aim.pos.horizontal * aim.pos.depth).to_string();
+}
+
 pub fn solution() -> Solution {
-    Solution { part1: Some(part1), part2: None }
+    Solution { part1: Some(part1), part2: Some(part2) }
 }
