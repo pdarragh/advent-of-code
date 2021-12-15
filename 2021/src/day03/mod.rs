ff15228e92d3801bf2989d466010675d57cf33eb
Pierce Darragh <pdarragh@Pierces-MacBook-Pro.local>
2021-12-14 21:13:34 -0500

    2021/day03: implement part 2

--- 2021/src/day03/mod.rs
+++ 2021/src/day03/mod.rs
@@ -4,19 +4,18 @@ use std::fs;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-fn flip_bit(b: i8) -> i8 {
+fn flip_bit(b: i32) -> i32 {
     if b == 0 { 1 } else if b == 1 { 0 } else { panic!("Invalid bit: {}", b) }
 }
 
-fn counts_to_binary(counts: &Vec<i32>, binary: &mut Vec<i8>) {
+fn counts_to_binary(counts: &Vec<i32>, binary: &mut Vec<i32>) {
     for &count in counts {
-        if count > 0 { binary.push(1) }
-        else if count < 0 { binary.push(0) }
-        else { panic!("Balanced binary value!") }
+        if count >= 0 { binary.push(1) }
+        else  { binary.push(0) }
     }
 }
 
-fn flip_binary(binary: &Vec<i8>) -> Vec<i8> {
+fn flip_binary(binary: &Vec<i32>) -> Vec<i32> {
     let mut flipped = binary.clone();
     for i in 0..flipped.len() {
         flipped[i] = flip_bit(flipped[i]);
@@ -24,11 +23,11 @@ fn flip_binary(binary: &Vec<i8>) -> Vec<i8> {
     flipped
 }
 
-fn binary_to_decimal(binary: &Vec<i8>) -> i32 {
+fn binary_to_decimal(binary: &Vec<i32>) -> i32 {
     let mut value: i32 = 0;
     let mut pow: i32 = 1;
     for b in binary.iter().rev() {
-        value += (*b as i32) * pow;
+        value += *b * pow;
         pow *= 2;
     }
     value
@@ -64,6 +63,61 @@ pub fn part1(file: &fs::File) -> String {
     (gamma * epsilon).to_string()
 }
 
+fn bit_to_count(b : &i32) -> i32 {
+    if *b == 1 { 1 } else { -1 }
+}
+
+fn compute_oxygen(values: &Vec<Vec<i32>>) -> i32 {
+    let mut remaining = values.clone();
+    let indices = values[0].len();
+    for idx in 0..indices {
+        let count = remaining.iter().fold(0, |a, v| a + bit_to_count(&v[idx]));
+        let keep = if count >= 0 { 1 } else { -1 };
+        remaining.retain(|v| v[idx] == keep);
+        if remaining.len() == 1 { break }
+    }
+    if remaining.len() != 1 {
+        panic!("Problem in oxygen calculation; {} items remaining.", remaining.len());
+    } else {
+        let mut binary = Vec::with_capacity(indices);
+        counts_to_binary(&remaining[0], &mut binary);
+        binary_to_decimal(&binary)
+    }
+}
+
+fn compute_co2(values: &Vec<Vec<i32>>) -> i32 {
+    let mut remaining = values.clone();
+    let indices = values[0].len();
+    for idx in 0..indices {
+        let count = remaining.iter().fold(0, |a, v| a + bit_to_count(&v[idx]));
+        let keep = if count < 0 { 1 } else { -1 };
+        remaining.retain(|v| v[idx] == keep);
+        if remaining.len() == 1 { break }
+    }
+    if remaining.len() != 1 {
+        panic!("Problem in CO2 calculation; {} items remaining.", remaining.len());
+    } else {
+        let mut binary = Vec::with_capacity(indices);
+        counts_to_binary(&remaining[0], &mut binary);
+        binary_to_decimal(&binary)
+    }
+}
+
+fn oxygen_and_co2(values: &Vec<Vec<i32>>) -> (i32, i32) {
+    (compute_oxygen(values), compute_co2(values))
+}
+
+pub fn part2(file: &fs::File) -> String {
+    let reader = BufReader::new(file);
+    let mut values = Vec::new();
+    for line in reader.lines().map(|l| l.unwrap()) {
+        let value = Vec::from(line.chars().map(char_to_bit).collect::<Vec<i32>>());
+        values.push(value);
+    }
+    let (oxygen, co2) = oxygen_and_co2(&values);
+    (oxygen * co2).to_string()
+}
+
 pub fn solution() -> Solution {
-    Solution { part1: Some(part1), part2: None }
+    Solution { part1: Some(part1), part2: Some(part2) }
 }
